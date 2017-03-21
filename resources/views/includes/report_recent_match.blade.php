535372ec22252e6ce2e4f3a5a96bfc14f468ac7d
Giovanni Schuoler <xxstryderxx@hotmail.com>
2017-03-21 15:13:17 -0700

    Added recent match view

---
+++ resources/views/includes/report_recent_match.blade.php
@@ -0,0 +1,31 @@
+
+	<div>
+		@php ($stats = json_decode(App::make("App\Http\Controllers\V1\ReportController")->getRecentMatchStats($summonerName)->content()))
+		<table>
+		<h4>Recent Match</h4>
+		{{-- json_encode($stats) --}}
+			<tr>
+				<td>Winner</td>
+				<td>Champion</td>
+				<td>Kills</td>
+				<td>Deaths</td>
+				<td>Assist</td>
+				<td>KDA</td>
+				<td>Champion Dmg</td>
+			</tr>
+			<tr>
+				<td>
+					@if ($stats->winner == 1) Victory
+					@elseif ($stats->winner == 0) Defeat
+					@else Leave/Remake
+					@endif
+				</td>
+				<td>{{ $stats->championId }}</td>
+				<td>{{ $stats->kills }}</td>
+				<td>{{ $stats->deaths }}</td>
+				<td>{{ $stats->assists }}</td>
+				<td>{{ round(($stats->kills + $stats->assists)/$stats->deaths, 2) }}</td>
+				<td>{{ $stats->totalDamageDealtToChampions }}</td>
+			</tr>
+		</table>
+	</div>
