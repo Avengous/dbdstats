2ca15203e579c028e92c8e21764ff613d93eeed9
Giovanni Schuoler <xxstryderxx@hotmail.com>
2017-04-21 15:27:18 -0700

    added more users to sidebar

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -13,20 +13,43 @@
 				</header>
 				<ul>
 					<strong>Roster</strong>
-					<li><a href="/summoner/Krystic">Krystic</a></li>
-					<li><a href="/summoner/Oliver Phillips">Oliver Phillips</a></li>
-					<li><a href="/summoner/Avengous">Avengous</a></li>
-					<li><a href="/summoner/Revenant">Revenant</a></li>
-					<li><a href="/summoner/Whambulance">Whambulance</a></li>
+					<li>
+						<a href="/summoner/Krystic">Krystic</a>|
+						<a href="/summoner/oroukaH">oroukaH</a>
+					</li>
+					<li>
+						<a href="/summoner/Oliver Phillips">Oliver Phillips</a>|
+						<a href="/summoner/NinjaBeaver">NinjaBeaver</a>
+					</li>
+					<li>
+						<a href="/summoner/Avengous">Avengous</a>|
+						<a href="/summoner/Stryder Edge">Stryder Edge</a>
+					</li>
+					<li>
+						<a href="/summoner/Revenant">Revenant</a>|
+						<a href="/summoner/Revenant Umbra">Revenant Umbra</a>|
+					</li>
+					<li>
+						<a href="/summoner/Whambulance">Whambulance</a>|
+						<a href="/summoner/Fantasizing">Fantasizing</a>
+					</li>
 				</ul>
 				<br/>
 				<ul>
 					<strong>Star Players</strong>
-					<li><a href="/summoner/Kytese">Kytese</a></li>
-					<li><a href="/summoner/Møchi">Møchi</a></li>
-					<li><a href="/summoner/Sweet Cherie">Sweet Cherie</a></li>
-					<li><a href="/summoner/Blîghtnîng">Blîghtnîng</a></li>
-					<li><a href="/summoner/Möther Theresa">Möther Theresa</a></li>
+					<li>
+						<a href="/summoner/Kytese">Kytese</a>|
+						<a href="/summoner/Anuru">Anuru</a>
+					</li>
+					<li>
+						<a href="/summoner/Møchi">Møchi</a>|
+						<a href="/summoner/Sweet Cherie">Sweet Cherie</a>
+					</li>
+					<li>
+						<a href="/summoner/Blîghtnîng">Blîghtnîng</a>|
+						<a href="/summoner/Möther Theresa">Möther Theresa</a>
+					</li>
+					<li><a href="/summoner/SAGA Gossip">SAGA Gossip</a></li>
 					<li><a href="/summoner/Wanganat0r">Wanganat0r</a></li>
 				</ul>
 			</nav>
